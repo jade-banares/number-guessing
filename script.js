7b0774d0ee0d5a337890e2063f8916a20a495adc
じぇいど <jade.arinal.banares@gmail.com>
2024-01-20 12:26:26 +0800

    change width

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
 
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').style.width = '10rem';
 });
 
 ///////////////////////////////////////
